e061a46bc67fdffba702ea2f71e2207e4333f440
bcorbold <bradencorbold@gmail.com>
2017-11-26 19:51:54 -0500

    [style] Clean up formatting

--- 4b03_solar_tracker.ino
+++ 4b03_solar_tracker.ino
@@ -5,6 +5,7 @@
 // Implementation of the Newton algorithm in C
 // http://deadline.3x.ro/newtoncode.html
 // instructor demonstration Azimuth / Zenith
+
 #define ASCIIZERO 48 // '0' = 48
 #define pi 3.1415926535897932384
 #define twoPI (2.0*pi)
@@ -29,23 +30,26 @@
 //Use decimal format (Latitude = 43 + 26.0181/60 = 43.434; Longitude = -1 * (79 degrees + 92.0892/60) = -80.535;)
 const double MCMASTERLATITUDE = 43.434;
 const double MCMASTERLONGITUDE = -80.535;
+
 struct cTime {
-int iYear;
-int iMonth;
-int iDay;
-double dHours;
-double dMinutes;
-double dSeconds;
+	int iYear;
+	int iMonth;
+	int iDay;
+	double dHours;
+	double dMinutes;
+	double dSeconds;
 };
+
 struct cLocation {
-double dLongitude;
-double dLatitude;
+	double dLongitude;
+	double dLatitude;
 };
 
 struct cSunCoordinates {
-double dZenithAngle;
-double dAzimuth;
+	double dZenithAngle;
+	double dAzimuth;
 };
+
 struct cTime utcTime;
 struct cLocation utcLocation;
 struct cSunCoordinates utcSunCoordinates;
@@ -56,47 +60,54 @@ double dAngleZE = ANGLEZEHOME;
 double dZenithNutPosition = ZENITHHOMENUTPOSITION;
 int iCurrentZEsteps = 8692;
 struct cTime currentTime;
-//simulating time cancer code
 
 
 //*************************************************************************************************************************************************
-void setup()
- {
+void setup() {
 	// setup serial communication
 	Serial.begin(9600);
 	Serial.println("SolarTracker v4.4");
 	Serial.println("Serial Connection initalized");
 	Serial.println("");
+
   currentTime.iYear = 2017;
   currentTime.iMonth = 11;
   currentTime.iDay = 27;
   currentTime.dHours = 15;
   currentTime.dMinutes = 12;
   currentTime.dSeconds = 15;
-	pinMode(MOTAZCWCCW,OUTPUT); // AZ motor
+
+	pinMode(MOTAZCWCCW, OUTPUT); // AZ motor
 	pinMode(MOTAZCLK, OUTPUT);
 	pinMode(OPTOPINAZ, INPUT); // opto slot sensor AZ
-	digitalWrite(MOTAZCWCCW,HIGH); // always go home CCW = HIGH
-	pinMode(MOTZECWCCW,OUTPUT); // EL motor
+	digitalWrite(MOTAZCWCCW, HIGH); // always go home CCW = HIGH
+
+	pinMode(MOTZECWCCW, OUTPUT); // EL motor
 	pinMode(MOTZECLK, OUTPUT);
 	pinMode(OPTOPINZE, INPUT); // opto slot sensor ZE
-	digitalWrite(MOTZECWCCW,HIGH); // always go home CCW = HIGH
+	digitalWrite(MOTZECWCCW, HIGH); // always go home CCW = HIGH
+
 	utcLocation.dLatitude = MCMASTERLATITUDE;
 	utcLocation.dLongitude = MCMASTERLONGITUDE;
+
 	Serial.println("Location: McMaster University, Hamilton, ON");
-	Serial.print("Latitude (Decimal Format): "); Serial.println(utcLocation.dLatitude);
-	Serial.print("Longitude (Decimal Format): "); Serial.println(utcLocation.dLongitude);
+	Serial.print("Latitude (Decimal Format): ");
+	Serial.println(utcLocation.dLatitude);
+	Serial.print("Longitude (Decimal Format): ");
+	Serial.println(utcLocation.dLongitude);
 	Serial.println("");
 	// home the AZ stepper by looking for blocked opto slot, when home = East = 90 degrees = 1800 steps
 	homeAzimuth();
 	// home the Zenith stepper
 	homeZenith();
-} // end setup()
+}
+
 //*************************************************************************************************************************************************
-void loop() { 
+void loop() {
 	getCurrentTime();
 	beginTracking();
 	Serial.println("");
+
 	delay(2000);
 	currentTime.dSeconds= currentTime.dSeconds + 2.5;
 	if (currentTime.dSeconds >= 60){
@@ -111,7 +122,8 @@ void loop() {
 		currentTime.dHours = 0;
 		currentTime.iDay++;
 	}
-} // end loop()
+}
+
 //*************************************************************************************************************************************************
 void getCurrentTime() {
 	utcTime.dSeconds = currentTime.dSeconds;
@@ -142,7 +154,8 @@ void getCurrentTime() {
 	if (utcTime.iYear < 10) Serial.print("0");
 	Serial.println(utcTime.iYear, DEC);
 	Serial.println("");
-} // end getCurrentTime()
+}
+
 //*************************************************************************************************************************************************
 void homeAzimuth() {
 	Serial.println("");
@@ -171,7 +184,7 @@ void homeAzimuth() {
 		iErrorAZFlag = 1;
 	} // end if
 }
- // end homeAzimuth()
+
 //*************************************************************************************************************************************************
 void homeZenith() {
 	Serial.println("Homing the Zenith-tracking stage to +90 degrees (Horizon)");
@@ -199,7 +212,8 @@ void homeZenith() {
 		// didn't get home
 		iErrorZEFlag = 1;
 	} // end if
-} // end homeZenith()
+}
+
 //*************************************************************************************************************************************************
 void beginTracking() {
 	Serial.println("Solar Tracking Initalized.");
@@ -231,7 +245,8 @@ void beginTracking() {
 		homeZenith();
 	} // end if
 	Serial.println("-----------------------------------------------------");
-} // end beginTracking()
+}
+
 //*************************************************************************************************************************************************
 void MoveMotorAZ(int iDeltaStepsAZ) {
 	int iCount;
@@ -256,7 +271,8 @@ void MoveMotorAZ(int iDeltaStepsAZ) {
 			delay(STEPDLY);
 		} // end for
 	} // end if
-} // end MoveMotorAZ()
+}
+
 //*************************************************************************************************************************************************
 void MoveMotorZE(int iDeltaStepsZE) {
 	int iCount;
@@ -281,10 +297,10 @@ void MoveMotorZE(int iDeltaStepsZE) {
 		delay(STEPDLY);
 		} // end for
 	} // end if
- } // end MoveMotorZE()
+ }
+
 //*************************************************************************************************************************************************
-void GetSunPos(struct cTime utcTime, struct cLocation utcLocation, struct cSunCoordinates *utcSunCoordinates)
-{
+void GetSunPos(struct cTime utcTime, struct cLocation utcLocation, struct cSunCoordinates *utcSunCoordinates) {
 	// Main variables
 	double dElapsedJulianDays;
 	double dDecimalHours;
@@ -292,54 +308,55 @@ void GetSunPos(struct cTime utcTime, struct cLocation utcLocation, struct cSunCo
 	double dEclipticObliquity;
 	double dRightAscension;
 	double dDeclination;
+
 	// Auxiliary variables
 	double dY;
 	double dX;
 	// Calculate difference in days between the current Julian Day
 	// and JD 2451545.0, which is noon 1 January 2000 Universal Time
-	 {
-	double dJulianDate;
-	long int liAux1;
-	long int liAux2;
-	// Calculate time of the day in UT decimal hours
-	dDecimalHours = utcTime.dHours + (utcTime.dMinutes+ utcTime.dSeconds / 60.0 ) / 60.0;
-	// Calculate current Julian Day
-	liAux1 =(utcTime.iMonth-14)/12;
-	liAux2=(1461*(utcTime.iYear + 4800 + liAux1))/4.0 + (367*(utcTime.iMonth- 2-12*liAux1))/12.0- (3*((utcTime.iYear + 4900+ liAux1)/100.0))/4.0+utcTime.iDay-32075;
-	dJulianDate=(double)(liAux2)-0.5+dDecimalHours/24.0;
-	// Calculate difference between current Julian Day and JD 2451545.0
-	 dElapsedJulianDays = dJulianDate-2451545.0;
-	 }
+	{
+		double dJulianDate;
+		long int liAux1;
+		long int liAux2;
+		// Calculate time of the day in UT decimal hours
+		dDecimalHours = utcTime.dHours + (utcTime.dMinutes+ utcTime.dSeconds / 60.0 ) / 60.0;
+		// Calculate current Julian Day
+		liAux1 =(utcTime.iMonth-14)/12;
+		liAux2=(1461*(utcTime.iYear + 4800 + liAux1))/4.0 + (367*(utcTime.iMonth- 2-12*liAux1))/12.0- (3*((utcTime.iYear + 4900+ liAux1)/100.0))/4.0+utcTime.iDay-32075;
+		dJulianDate=(double)(liAux2)-0.5+dDecimalHours/24.0;
+		// Calculate difference between current Julian Day and JD 2451545.0
+		dElapsedJulianDays = dJulianDate-2451545.0;
+	}
 	// Calculate ecliptic coordinates (ecliptic longitude and obliquity of the
 	// ecliptic in radians but without limiting the angle to be less than 2*Pi
 	// (i.e., the result may be greater than 2*Pi)
-	 {
-	double dMeanLongitude;
-	double dMeanAnomaly;
-	double dOmega;
-	 dOmega=2.1429-0.0010394594*dElapsedJulianDays;
-	 dMeanLongitude = 4.8950630+ 0.017202791698*dElapsedJulianDays; // Radians
-	 dMeanAnomaly = 6.2400600+ 0.0172019699*dElapsedJulianDays;
-	 dEclipticLongitude = dMeanLongitude + 0.03341607*sin( dMeanAnomaly )
-	 + 0.00034894*sin( 2*dMeanAnomaly )-0.0001134
-	 -0.0000203*sin(dOmega);
-	 dEclipticObliquity = 0.4090928 - 6.2140e-9*dElapsedJulianDays
-	 +0.0000396*cos(dOmega);
-	 }
+	{
+		double dMeanLongitude;
+		double dMeanAnomaly;
+		double dOmega;
+		 dOmega=2.1429-0.0010394594*dElapsedJulianDays;
+		 dMeanLongitude = 4.8950630+ 0.017202791698*dElapsedJulianDays; // Radians
+		 dMeanAnomaly = 6.2400600+ 0.0172019699*dElapsedJulianDays;
+		 dEclipticLongitude = dMeanLongitude + 0.03341607*sin( dMeanAnomaly )
+		 + 0.00034894*sin( 2*dMeanAnomaly )-0.0001134
+		 -0.0000203*sin(dOmega);
+		 dEclipticObliquity = 0.4090928 - 6.2140e-9*dElapsedJulianDays
+		 +0.0000396*cos(dOmega);
+	}
 	// Calculate celestial coordinates ( right ascension and declination ) in radians
 	// but without limiting the angle to be less than 2*Pi (i.e., the result may be
 	// greater than 2*Pi)
-	 {
-	double dSin_EclipticLongitude;
-	 dSin_EclipticLongitude= sin( dEclipticLongitude );
-	 dY = cos( dEclipticObliquity ) * dSin_EclipticLongitude;
-	 dX = cos( dEclipticLongitude );
-	 dRightAscension = atan2( dY,dX );
-	if( dRightAscension < 0.0 ) dRightAscension = dRightAscension + twoPI;
-	 dDeclination = asin( sin( dEclipticObliquity )*dSin_EclipticLongitude );
-	 }
-	// Calculate local coordinates ( azimuth and zenith angle ) in degrees
-	 {
+	{
+		double dSin_EclipticLongitude;
+		dSin_EclipticLongitude= sin( dEclipticLongitude );
+		dY = cos( dEclipticObliquity ) * dSin_EclipticLongitude;
+		dX = cos( dEclipticLongitude );
+		dRightAscension = atan2( dY,dX );
+		if( dRightAscension < 0.0 ) dRightAscension = dRightAscension + twoPI;
+		dDeclination = asin( sin( dEclipticObliquity )*dSin_EclipticLongitude );
+		}
+		// Calculate local coordinates ( azimuth and zenith angle ) in degrees
+	{
 	double dGreenwichMeanSiderealTime;
 	double dLocalMeanSiderealTime; 
 	double dLatitudeInRadians;
@@ -348,18 +365,15 @@ void GetSunPos(struct cTime utcTime, struct cLocation utcLocation, struct cSunCo
 	double dSin_Latitude;
 	double dCos_HourAngle;
 	double dParallax;
-	dGreenwichMeanSiderealTime = 6.6974243242 +
-	0.0657098283*dElapsedJulianDays
-	+ dDecimalHours;
-	dLocalMeanSiderealTime = (dGreenwichMeanSiderealTime*15
-	+ utcLocation.dLongitude)*rad;
+
+	dGreenwichMeanSiderealTime = 6.6974243242 + 0.0657098283*dElapsedJulianDays + dDecimalHours;
+	dLocalMeanSiderealTime = (dGreenwichMeanSiderealTime*15 + utcLocation.dLongitude)*rad;
 	dHourAngle = dLocalMeanSiderealTime - dRightAscension;
 	dLatitudeInRadians = utcLocation.dLatitude*rad;
 	dCos_Latitude = cos( dLatitudeInRadians );
 	dSin_Latitude = sin( dLatitudeInRadians );
 	dCos_HourAngle= cos( dHourAngle );
-	utcSunCoordinates->dZenithAngle = (acos( dCos_Latitude*dCos_HourAngle
-	*cos(dDeclination) + sin( dDeclination )*dSin_Latitude));
+	utcSunCoordinates->dZenithAngle = (acos( dCos_Latitude*dCos_HourAngle *cos(dDeclination) + sin( dDeclination )*dSin_Latitude));
 	dY = -sin( dHourAngle );
 	dX = tan( dDeclination )*dCos_Latitude - dSin_Latitude*dCos_HourAngle;
 	utcSunCoordinates->dAzimuth = atan2( dY, dX );
@@ -369,10 +383,9 @@ void GetSunPos(struct cTime utcTime, struct cLocation utcLocation, struct cSunCo
 	// Parallax Correction
 	dParallax=(dEarthRadius/dAstroUnit)
 	*sin(utcSunCoordinates->dZenithAngle);
-	utcSunCoordinates->dZenithAngle= (utcSunCoordinates->dZenithAngle
-	+ dParallax)/rad;
+	utcSunCoordinates->dZenithAngle= (utcSunCoordinates->dZenithAngle + dParallax)/rad;
 	}
-} // end GetSunPos()
+}
 
 //*************************************************************************************************************************************************
 int getZenithSteps(double theta) {
@@ -383,9 +396,10 @@ int getZenithSteps(double theta) {
 	int iterations; // Actual number of iterations
 	int converged; // Whether iteration converged
 	double thetaRAD = theta * rad;
-	 beta0 = 0;
-	 epsilon = 0.00001;
-	 maxIterations = 10000;
+
+	beta0 = 0;
+	epsilon = 0.00001;
+	maxIterations = 10000;
 	double nutPosition =-1;
 	// Find the first positive solution.
 	while (nutPosition <= 0) { 
@@ -393,6 +407,7 @@ int getZenithSteps(double theta) {
 		nutPosition = 25*cos(thetaRAD) + 53*cos(thetaRAD+PI+beta);
 		beta0++;
 	}
+
 	if (converged) {
 		Serial.print("Newton algorithm converged after this many steps: ");
 		Serial.println(iterations, DEC);
@@ -400,12 +415,12 @@ int getZenithSteps(double theta) {
 		Serial.print(beta, DEC);
 		Serial.print(") = ");
 		Serial.println(f(thetaRAD, beta), DEC);
-	 }
-	else {
+	} else {
 		Serial.println("Newton's method algorithm didn't converge");
 		Serial.print("The final estimate was: ");
 		Serial.println(iterations, DEC);
 	}
+
 	// Serial.println("");
 	// Serial.print("|AD| length (mm): ");
 	// Serial.println(nutPosition, DEC);
@@ -413,10 +428,10 @@ int getZenithSteps(double theta) {
 	//calculate and return number of steps
 	// 0.004mm per step
 	return ((int)(nutPosition / 0.004));
-} // end getZenithSteps
+}
+
 //*************************************************************************************************************************************************
-double newtonsMethod(double beta0, double epsilon, int maxIterations,
-	int* iterations_p, int* converged_p, double thetaRAD ) {
+double newtonsMethod(double beta0, double epsilon, int maxIterations,	int* iterations_p, int* converged_p, double thetaRAD ) {
 	double beta = beta0;
 	double beta_prev;
 	int iter = 0;
@@ -431,13 +446,14 @@ double newtonsMethod(double beta0, double epsilon, int maxIterations,
 	*converged_p = 0;
 	*iterations_p = iter;
 	return beta;
-} // end newtonsMethod
+}
+
 //*************************************************************************************************************************************************
 double f(double thetaRAD, double beta) {
 	return 15 + 25*sin(thetaRAD) - 53*sin(thetaRAD + beta);
-} // end f
+}
+
 //*************************************************************************************************************************************************
 double f_prime(double thetaRAD, double beta) {
 	return -53*cos(thetaRAD + beta); //the derivative
-} // end f_prime
-//*************************************************************************************************************************************************
+}
